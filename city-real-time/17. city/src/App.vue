4beed59cc47e1f15ab3423e0a60f92eba7c3b8a9
chenxiaolong <joysonchen@qq.com>
2024-02-07 11:38:44 +0800

    first commit

---
+++ city-real-time/17. city/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initCity } from './enter'
+onMounted(() => {
+  // 初始化三维场景
+  initCity();
+})
+</script>
+
+<template>
+  <canvas id="webgl">浏览器不支持canvas，请切换浏览器重试</canvas>
+</template>
+
+<style scoped>
+
+</style>
